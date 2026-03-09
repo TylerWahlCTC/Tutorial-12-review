21e4bc866da31dd35a2b4ce236569d2cdc83b9de
TRWahl1s <TRWahl1s@capetigers.com>
2026-03-09 10:01:45 -0500

    Finished

--- js/bc_keys.js
+++ js/bc_keys.js
@@ -31,40 +31,91 @@ window.addEventListener("load", findKeyWords);
 window.addEventListener("load", makeKeyStyles);
 
 function findKeyWords() {
-   // Create aside
-   var keyword = document.createElement("aside");
-   keyword.id = "keywords";
-   // Create and append heading
-   var heading = document.createElement("h1");
-   heading.textContent = "Keyword List";
-   keyword.appendChild(heading);
-   // Create and append ordered list
-   var keywordList = document.createElement("ol");
-   keyword.appendChild(keywordList);
+  var keywords = document.getElementById("keywords");
 
-   var keyWordElems = document.querySelectorAll("dfn");
-   var keyWords = [];
-   for (var i = 0; i < keyWordElems.length; i++) {
-      keyWords[i] = keyWordElems[i].innerHTML;
-      var linkID = [];
-      linkID[i] = replaceWS(keyWords[i]);
-      keyWordElems[i].setAttribute("id", linkID[i]);
-   }
-   keyWords.sort;
-   // console.log("Test for keywords", keyWords);
+  /* Locates the keywords from the document to create a keyword list */
+  var keywords = document.createElement("aside");
+  keywords.setAttribute("id", "keywords");
+
+  var mainHeading = document.createElement("h1");
+  var outlineList = document.createElement("ol");
+
+  mainHeading.innerHTML = "Keyword List";
+  keywords.appendChild(mainHeading);
+  keywords.appendChild(outlineList);
+
+  var keyWordElems = document.querySelectorAll("dfn");
+  var keyWords = [];
+
+  for (var i = 0; i < keyWordElems.length; i++) {
+    keyWords[i] = keyWordElems[i].innerHTML;
+    var linkID = [];
+    linkID[i] = replaceWS(keyWords[i]);
+
+    keyWordElems[i].setAttribute("id", linkID[i]);
+  }
+  keyWords.sort();
+
+  for (var i = 0; i < keyWords.length; i++) {
+    var keyWordListItem = document.createElement("li");
+    var keyWordLink = document.createElement("a");
+
+    keyWordLink.innerHTML = keyWords[i];
+
+    linkID[i] = replaceWS(keyWords[i]);
+
+    keyWordLink.setAttribute("id", "#" + linkID[i]);
+
+    keyWordListItem.append(keyWordLink);
+    outlineList.append(keyWordListItem);
+  }
+
+  keywords.appendChild(mainHeading);
+  keywords.appendChild(outlineList);
+
+  var target = document.getElementById("doc");
+  target.insertBefore(keywords, target.firstChild);
 }
 
 function makeKeyStyles() {
-
+  document.styleSheets[document.styleSheets.length - 1].insertRule(
+    "aside#keywords { \
+         border: 3px solid rgb(101, 101, 101); \
+         float: right; \
+         margin: 20px 0px 20px 20px \
+         padding: 10px; \
+         width: 320px; \
+     }",
+  );
+  document.styleSheets[document.styleSheets.length - 1].insertRule(
+    "aside#keywords h1 { \
+     font-size:2em; \
+     margin: 5px; \
+     text-align: center; \
+  }",
+  );
+  document.styleSheets[document.styleSheets.length - 1].insertRule(
+    "aside#keywords ol { \
+   margin-left: 20px; \
+   font-size:1.2em; \
+  }",
+  );
+  document.styleSheets[document.styleSheets.length - 1].insertRule(
+    "aside#keywords ol li { \
+    line-height: 1.5em; \
+  }",
+  );
+  document.styleSheets[document.styleSheets.length - 1].insertRule(
+    "aside#keywords ol li a { \
+    color: rgb(101, 101, 101); \
+    text-decoration: none; \
+  }",
+  );
 }
-
-
-
-
 
 /* Supplied Functions */
 
 function replaceWS(textStr) {
-   var revText = textStr.replace(/\s+/g,"_");
-   return revText;
+  var revText = textStr.replace(/\s+/g, "_");
+  return revText;
 }
